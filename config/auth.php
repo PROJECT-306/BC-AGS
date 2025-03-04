7473bab571790656cf6c32550375ab88f117daf9
Learn From Xavi <119419905+xaviworks@users.noreply.github.com>
2025-03-04 08:20:03 +0800

    Update auth.php

--- config/auth.php
+++ config/auth.php
@@ -4,7 +4,7 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Authentication Defaultss
+    | Authentication Defaults
     |--------------------------------------------------------------------------
     |
     | This option defines the default authentication "guard" and password
