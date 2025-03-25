da261b9a06ade6178abb0d326fe86b0c2c3cdd11
TimenyaSupangibab <njpalicte12345@gmail.com>
2025-03-25 23:51:44 +0800

    Normalize line endings to LF

--- database/migrations/2025_03_25_120624_create_instructors_table.php
+++ database/migrations/2025_03_25_120624_create_instructors_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::create('instructors', function (Blueprint $table) {
             $table->id('instructor_id');
             $table->unsignedBigInteger('user_id')->unique();
-            $table->foreign('user_id')->references('user_id')->on('users')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->unsignedBigInteger('department_id')->nullable();
             $table->foreign('department_id')->references('department_id')->on('departments')->onDelete('set null');
             $table->timestamps();
