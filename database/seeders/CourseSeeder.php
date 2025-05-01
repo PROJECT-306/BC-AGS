8ad26ebd99366e04cb5decb46def3c9a2bc379a7
Junn Xavier Adalid <xaviadalid959@gmail.com>
2025-05-02 00:51:19 +0800

    update

--- database/seeders/CourseSeeder.php
+++ database/seeders/CourseSeeder.php
@@ -2,17 +2,55 @@
 
 namespace Database\Seeders;
 
-use App\Models\Course;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Course;
 
 class CourseSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run()
     {
-        Course::factory()->count(10)->create();
+        // Inserting courses into the ASBME department
+        Course::create([
+            'course_name' => 'BSHM',
+            'department_id' => 2, // ASBME department ID, replace if needed
+        ]);
+        Course::create([
+            'course_name' => 'BSPSYCH',
+            'department_id' => 2,
+        ]);
+        Course::create([
+            'course_name' => 'AB THEO',
+            'department_id' => 2,
+        ]);
+        Course::create([
+            'course_name' => 'BEED',
+            'department_id' => 2,
+        ]);
+        Course::create([
+            'course_name' => 'BSBA',
+            'department_id' => 2,
+        ]);
+        Course::create([
+            'course_name' => 'BSIT',
+            'department_id' => 2,
+        ]);
+        Course::create([
+            'course_name' => 'BSMLS',
+            'department_id' => 2,
+        ]);
+        Course::create([
+            'course_name' => 'BS PHARMA',
+            'department_id' => 2,
+        ]);
+        Course::create([
+            'course_name' => 'BSN',
+            'department_id' => 2,
+        ]);
+        Course::create([
+            'course_name' => 'DOCTOR OF MEDICINE',
+            'department_id' => 2,
+        ]);
     }
 }
+
+
